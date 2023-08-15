d0f676336927019a9b65c0dfb9adadd8de59d2db
Vasakot <vasily.kotkoff@yandex.ru>
2023-08-15 21:59:53 +0300

    ping

--- src/section_1/section1.2/Program.cs
+++ src/section_1/section1.2/Program.cs
@@ -2,4 +2,16 @@
 
 // TODO for Coding Challenge Start here on starting-point branch
 
+using StackExchange.Redis;
+Console.WriteLine("Hello Redis!");
+
+var muxer = ConnectionMultiplexer.Connect(new ConfigurationOptions
+{
+    EndPoints = { "localhost:6379" }
+});
+
+var db = muxer.GetDatabase();
+var res = db.Ping();
+Console.WriteLine($"The ping took: {res.TotalMilliseconds} ms");
+
 // end programming challenge
