3055e21bc81580db0ad98417146f8d73375f187a
Vasakot <vasily.kotkoff@yandex.ru>
2023-08-17 19:58:21 +0300

    1.5 Un-Pipelined

--- src/section_1/section1.5/Program.cs
+++ src/section_1/section1.5/Program.cs
@@ -10,6 +10,13 @@ var muxer = ConnectionMultiplexer.Connect(options);
 var db = muxer.GetDatabase();
 
 var stopwatch = Stopwatch.StartNew();
-// TODO for Coding Challenge Start here on starting-point branch
 
+// TODO for Coding Challenge Start here on starting-point branch
+// un-pipelined commands incur the added cost of an extra round trip
+for (var i = 0; i < 1000; i++)
+{
+    await db.PingAsync();
+}
+
+Console.WriteLine($"1000 un-pipelined commands took: {stopwatch.ElapsedMilliseconds}ms to execute");
 // end Challenge
