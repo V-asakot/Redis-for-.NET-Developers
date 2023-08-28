e4810b7abf34ae171d515d0b59e3b0a67c10a225
Vasakot <vasily.kotkoff@yandex.ru>
2023-08-28 21:44:35 +0300

    secton 2.2 -2.5

--- src/section_2/section2.2/Program.cs
+++ src/section_2/section2.2/Program.cs
@@ -3,5 +3,30 @@
 var muxer = ConnectionMultiplexer.Connect("localhost");
 var db = muxer.GetDatabase();
 // TODO for Coding Challenge Start here on starting-point branch
+var fruitKey = "fruits";
+var vegetableKey = "vegetables";
+db.KeyDelete(new RedisKey[] { fruitKey, vegetableKey });
 
+db.ListLeftPush(fruitKey, new RedisValue[] { "Banana", "Mango", "Apple", "Pepper", "Kiwi", "Grape" });
+Console.WriteLine($"The first fruit in the list is: {db.ListGetByIndex(fruitKey, 0)}");
+Console.WriteLine($"The last fruit in the list is:  {db.ListGetByIndex(fruitKey, -1)}");
+db.ListRightPush(vegetableKey, new RedisValue[] { "Potato", "Carrot", "Asparagus", "Beet", "Garlic", "Tomato" });
+Console.WriteLine($"The first vegetable in the list is: {db.ListGetByIndex(vegetableKey, 0)}");
+Console.WriteLine($"The last vegetable in the list is:  {db.ListGetByIndex(vegetableKey, -1)}");
+Console.WriteLine($"Fruit indexes 0 to -1: {string.Join(", ", db.ListRange(fruitKey))}");
+Console.WriteLine($"Vegetables index 0 to -2: {string.Join(", ", db.ListRange(vegetableKey, 0, -2))}");
+db.ListMove(vegetableKey, fruitKey, ListSide.Right, ListSide.Left);
+
+Console.WriteLine("Enqueuing Celery");
+db.ListLeftPush(vegetableKey, "Celery");
+Console.WriteLine($"Dequeued: {db.ListRightPop(vegetableKey)}");
+
+Console.WriteLine("Pushing Grapefruit");
+db.ListLeftPush(fruitKey, "Grapefruit");
+Console.WriteLine($"Popping Fruit: {string.Join(",", db.ListLeftPop(fruitKey, 2))}");
+
+Console.WriteLine($"Position of Mango: {db.ListPosition(fruitKey, "Mango")}");
+
+Console.WriteLine($"There are {db.ListLength(fruitKey)} fruits in our Fruit List");
+Console.ReadKey();
 // end coding challenge
--- src/section_2/section2.3/Program.cs
+++ src/section_2/section2.3/Program.cs
@@ -3,5 +3,24 @@ var muxer = ConnectionMultiplexer.Connect("localhost");
 var db = muxer.GetDatabase();
 
 // TODO for Coding Challenge Start here on starting-point branch
+var allUsersSet = "users";
+var activeUsersSet = "users:state:active";
+var inactiveUsersSet = "users:state:inactive";
+var offlineUsersSet = "users:state:offline";
+db.KeyDelete(new RedisKey[] { allUsersSet, activeUsersSet, inactiveUsersSet, offlineUsersSet });
 
+db.SetAdd(activeUsersSet, new RedisValue[] { "User:1", "User:2" });
+db.SetAdd(inactiveUsersSet, new RedisValue[] { "User:3", "User:4" });
+db.SetAdd(offlineUsersSet, new RedisValue[] { "User:5", "User:6", "User:7" });
+
+db.SetCombineAndStore(SetOperation.Union, allUsersSet, new RedisKey[] { activeUsersSet, inactiveUsersSet, offlineUsersSet });
+
+var user6Offline = db.SetContains(offlineUsersSet, "User:6");
+Console.WriteLine($"User:6 offline: {user6Offline}");
+Console.WriteLine($"All Users In one shot: {string.Join(", ", db.SetMembers(allUsersSet))}");
+Console.WriteLine($"All Users with scan  : {string.Join(", ", db.SetScan(allUsersSet))}");
+Console.WriteLine("Moving User:1 from active to offline");
+var moved = db.SetMove(activeUsersSet, offlineUsersSet, "User:1");
+Console.WriteLine($"Move Successful: {moved}");
+Console.ReadKey();
 // end coding challenge
